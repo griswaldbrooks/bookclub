522b15be2650c341ea0b40be19e3e3fb1664751b
Griswald Brooks <griswald.brooks@picknik.ai>
2022-07-30 03:35:13 +0000

    Add twist and screw

--- velocity_kinematics/src/main.rs
+++ velocity_kinematics/src/main.rs
@@ -1,11 +1,36 @@
-use nalgebra::{Scalar, Vector3};
+extern crate nalgebra as na;
+// use na::{Scalar, Vector3};
 
-fn print_vector<T: Scalar>(m: &Vector3<T>) {
-  println!("{:?}", m)
+// S = [-ω x q, ω]^T = [v, ω]^T
+//   [ω] = skew symmetric matrix
+//   R = I + [ω]sinθ + [ω]^2(1 - cosθ) // rodriguez
+//   p = (Iθ + (1 − cos θ)[ω] + (θ − sin θ)[ω]^2)v
+//   e^[S]θ = [R p]
+//            [0 1]
+//   Ad_e^[S]θ = [ R   0]
+//               [[p]R R]
+
+#[derive(Debug)]
+struct Twist {
+    angular: na::Vector3<f64>,
+    linear: na::Vector3<f64>,
+}
+
+struct Screw {
+    axis: Twist,
+}
+
+impl Screw {
+    fn from(&self, axis: Twist) -> Screw {
+        Screw { axis }
+    }
 }
 
 fn main() {
-  let v1 = Vector3::new(1, 2, 3);
+    let v1 = Twist {
+        angular: na::Vector3::<f64>::new(1., 2., 3.),
+        linear: na::Vector3::<f64>::new(4., 5., 6.),
+    };
 
-  print_vector(&v1);
+    println!("{:?}", &v1);
 }
